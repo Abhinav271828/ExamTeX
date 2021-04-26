b5658881e54bc19434b9d2ee583c5d196c135a48
Abhinav271828 <abhinav.menon@students.iiit.ac.in>
2021-04-26 20:44:11 +0530

    Convert sample files to txt; added fn defns to adt.h & added stack.h

--- adt.h
+++ adt.h
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef __ADT__
+#define __ADT__
+
 //Header file defining ADTs
 
 typedef struct Bank Bank;
@@ -8,7 +14,7 @@ typedef struct NUM NUM;
 typedef struct Paper Paper;
 typedef struct Request Request;
 
-//Question bank file; arrays of questions for each type
+//Question bank file; linked list of questions for each type
 struct Bank
 {
     struct MCQ* mcq_list;
@@ -26,7 +32,8 @@ struct MCQ
     int no_ops;
     float diff;
     float score;
-}
+    struct MCQ* next;
+};
 
 //Fill in the Blank type question
 struct FITB
@@ -35,7 +42,8 @@ struct FITB
     char* ans;
     float diff;
     float score;
-}
+    struct FITB* next;
+};
 
 //True/False type question
 struct TF
@@ -44,7 +52,8 @@ struct TF
     char ans;
     float diff;
     float score;
-}
+    struct TF* next;
+};
 
 //Numerical type question
 struct NUM
@@ -53,16 +62,17 @@ struct NUM
     int ans;
     float diff;
     float score;
-}
+    struct NUM* next;
+};
 
 //Question paper file; list of requests, one for each type
 struct Paper
 {
-    struct Request mcq_reqs;
+    struct Request mcq_reqs[10];
     struct Request fitb_reqs;
     struct Request tf_reqs;
     struct Request num_reqs;
-}
+};
 
 //Each request
 struct Request
@@ -71,4 +81,21 @@ struct Request
     float diff_lb;
     float diff_ub;
     int no_ops;
-}
+};
+
+//Functions
+//Initialisation
+Bank* create_bank();
+MCQ*  create_MCQ();
+FITB* create_FITB();
+TF*   create_TF();
+NUM*  create_NUM();
+
+//Parsing QBank
+Bank* parse_bank();
+MCQ*  parse_MCQ();
+FITB* parse_FITB();
+TF*   parse_TF();
+NUM*  parse_NUM();
+
+#endif
---
+++ stack.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef __STACK
+#define __STACK
+
+typedef struct Node Node;
+typedef struct Node* PtrToNode;
+typedef PtrToNode Stack;
+
+
+struct Node
+{
+    char part; // part = 't' if \begin{type} has been pushed; 'q' if \begin{question} has been pushed 
+    struct Node* next;
+};
+
+Stack create_empty();
+PtrToNode create_node(char val);
+void push(Stack S, char val);
+char pop(Stack S);
+
+#endif
